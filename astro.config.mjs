cce4e9edb0ee61bbc98ec079e857cecc2366198c
Allbless Lema <allblesslema18@gmail.com>
2026-05-25 00:23:27 -0400

    fix astro config merge conflict, add missing book covers

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://allblesslema.github.io',
-<<<<<<< HEAD
-  base: 'Personal-Site',
-=======
   base: '/Personal-Site',
->>>>>>> 2f681fb (fixed a few things)
 });
