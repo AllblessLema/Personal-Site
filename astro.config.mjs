02be573adcd790a4d50553eff6e9e2d423b50eff
Allbless Lema <allblesslema18@gmail.com>
2026-05-25 00:03:33 -0400

    fixed a few things

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://allblesslema.github.io',
+<<<<<<< HEAD
   base: 'Personal-Site',
+=======
+  base: '/Personal-Site',
+>>>>>>> 2f681fb (fixed a few things)
 });
