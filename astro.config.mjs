c3fb3b51ff87dda982f623c42b7fd3bd4bc25c87
Allbless Lema <allblesslema18@gmail.com>
2026-05-24 23:17:26 -0400

    Add site and base configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://allblesslema.github.io',
+  base: 'Personal-Site',
+});
